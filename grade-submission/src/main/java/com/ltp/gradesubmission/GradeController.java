add88b5cf543d4f803e92b4ac58151d982f0369e
iki <iki.jito@gmail.com>
2022-10-09 03:06:12 +0800

    revert back to modern method

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,21 +1,17 @@
 package com.ltp.gradesubmission;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GradeController {
 
     @GetMapping("/grades")
-    public ModelAndView getGrades() {
-        Map<String, Grade> model = new HashMap<String, Grade>();
-        model.put("first", new Grade("happy potter", "Potions", "C-"));
-        model.put("second", new Grade("hermoine ganger", "Spells", "A+"));
-        return new ModelAndView("grades", model);
+    public String getGrades(Model model) {
+        model.addAttribute("first", new Grade("happy potter", "Potions", "C-"));
+        model.addAttribute("second", new Grade("hermoine ganger", "Spells", "A+"));
+        return "grades";
     }
 
 }
