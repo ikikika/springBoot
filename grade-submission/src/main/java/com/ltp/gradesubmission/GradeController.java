2994746e62ce8e0fed5d62a5ec26c07296e6d8db
iki <iki.jito@gmail.com>
2022-10-10 06:00:18 +0800

    submit form, redirect to another route

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,19 +1,23 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-            new Grade("happy potter", "Potions and magic", "C-", 20),
-            new Grade("hermoine ganger", "Spells", "A+", 25),
-            new Grade("ron wesly", "Unknown", "B+", 23));
+    // List<Grade> studentGrades = Arrays.asList(
+    //         new Grade("happy potter", "Potions and magic", "C-", 20),
+    //         new Grade("hermoine ganger", "Spells", "A+", 25),
+    //         new Grade("ron wesly", "Unknown", "B+", 23));
+
+    List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
@@ -27,8 +31,14 @@ public class GradeController {
 
     @GetMapping("/form")
     public String gradeForm(Model model){
-        model.addAttribute("grade", new Grade("happy potter", "Potions and magic", "C-", 20));
+        model.addAttribute("grade", new Grade());
         return "form";
+    }
+
+    @PostMapping("handleSubmit")
+    public String submitForm(Grade grade){
+        studentGrades.add(grade);
+        return "redirect:/grades";
     }
 
 }
