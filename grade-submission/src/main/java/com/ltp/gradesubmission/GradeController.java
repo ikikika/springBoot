69f967b3e6cad2cf9f0d61aa5e0bb9ce7dc150d9
iki <iki.jito@gmail.com>
2022-10-10 05:09:48 +0800

    thymeleaf utility methods

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -9,7 +9,7 @@ public class GradeController {
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        model.addAttribute("first", new Grade("happy potter", "Potions", "C-", 20));
+        model.addAttribute("first", new Grade("happy potter", "Potions and magic", "C-", 20));
         model.addAttribute("second", new Grade("hermoine ganger", "Spells", "A+", 25));
         return "grades";
     }
