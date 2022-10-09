843880ebb515fbd5b2180a03222b8abd40c16bc9
iki <iki.jito@gmail.com>
2022-10-10 06:44:11 +0800

    thymeleaf get index, update

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -8,14 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
 
     // List<Grade> studentGrades = Arrays.asList(
-    //         new Grade("happy potter", "Potions and magic", "C-", 20),
-    //         new Grade("hermoine ganger", "Spells", "A+", 25),
-    //         new Grade("ron wesly", "Unknown", "B+", 23));
+    // new Grade("happy potter", "Potions and magic", "C-", 20),
+    // new Grade("hermoine ganger", "Spells", "A+", 25),
+    // new Grade("ron wesly", "Unknown", "B+", 23));
 
     List<Grade> studentGrades = new ArrayList<>();
 
@@ -30,15 +31,31 @@ public class GradeController {
     }
 
     @GetMapping("/form")
-    public String gradeForm(Model model){
-        model.addAttribute("grade", new Grade());
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        model.addAttribute("grade", getGradeIndex(name) == -1 ? new Grade() : studentGrades.get(getGradeIndex(name)));
         return "form";
     }
 
     @PostMapping("handleSubmit")
-    public String submitForm(Grade grade){
-        studentGrades.add(grade);
+    public String submitForm(Grade grade) {
+        int index = getGradeIndex(grade.getName());
+        System.out.println(index);
+        if (index == -1) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
+
         return "redirect:/grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
